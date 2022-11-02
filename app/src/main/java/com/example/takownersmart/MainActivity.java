9074f3a3cbebf6a33845951493ce4695d10f598c
panosbabo <panosbabo@hotmail.com>
2022-11-02 16:17:13 +0000

    Added Bottom Navbar

--- app/src/main/java/com/example/takownersmart/MainActivity.java
+++ app/src/main/java/com/example/takownersmart/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.takownersmart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.takownersmart.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    Intent homepage = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(homepage);
+                    break;
+                case R.id.profile:
+                    changeScreen(new profile_page());
+                    break;
+                case R.id.settings:
+                    changeScreen(new settings_page());
+                    break;
+            }
+            return true;
+        });
+
+    }
+
+    private void changeScreen(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
